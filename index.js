340d949e9ecade8ab1244786508ff590f489b371
Jacob Archambault <jacob.archambault@waystar.com>
2021-03-06 09:57:59 -0500

    Add get name to main page greeting

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 var express = require('express');
-var app = express();          // create the express app
+var app = express();
 
-app.get('/', function (req, res) {
-    res.send('This is the default route.')
-})
+app.get('/', function(req, res) {
+    var name = req.query.name; //e.g. localhost:3000?name=larry
+    if (name) {
+        res.send('Hi, ' + name + ", nice to meet you.");
+    }
+    else {
+        res.send('Welcome, guest!');
+    }
+});
 
-app.get('/about', function (req, res) {
-    res.send('This is the about page.')
-})
+app.get('/about', function(req, res) {
+    res.send('This is the about page.');
+});
+
+app.post('/login', function(req, res) {
+    res.send('This is the login page.');
+});
+
+app.use(function (req, res) {
+    res.status(404).send("Sorry, no such page!")
+});
 
 app.listen(3000, function() {
-    console.log('Listening on port 3000, ctrl-c to quit');
+    console.log('Listening on port 3000, ctrl-c to quit.');
 });
